257ce70c4be3a7327d9c0ee2a83dbf8972f08344
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2014-07-24 14:55:39 +0200

    Add tests for biding, unbiding and communication with a component

--- tests/component_SUITE.erl
+++ tests/component_SUITE.erl
@@ -25,7 +25,11 @@
 %%--------------------------------------------------------------------
 
 all() ->
-    [client_to_component].
+    [client_to_component,
+     component_to_component,
+     multiple_bind,
+     late_multiple_bind,
+     component_unbind].
 
 suite() ->
     escalus:suite().
@@ -61,11 +65,77 @@ end_per_testcase(CaseName, Config) ->
 client_to_component(Config) ->
     Options = [{port, 8888}, {server, <<"localhost">>},
                {from, <<"component.localhost">>},
-               {bind_hostname, <<"component1.localhost">>}],
+               {bind_hostname, [H = <<"component1.localhost">>]}],
     {ok, Component} = escalus_client:start_component(Config, Options),
     {ok, Alice} = escalus_client:start(Config, alice, <<"res1">>),
-    escalus_client:send(Alice, escalus_stanza:chat_to(Component, <<"Hi!">>)),
+    escalus_client:send(Alice, escalus_stanza:chat_to(H, <<"Hi!">>)),
     escalus_assert:is_chat_message(
       <<"Hi!">>, escalus_client:wait_for_stanza(Component)),
     [escalus_client:stop(C) || C <- [Component, Alice]].
+
+component_to_component(Config) ->
+    Options1 = [{port, 8888}, {server, <<"localhost">>},
+               {from, <<"component1.localhost">>},
+               {bind_hostname, [H1 = <<"component1.localhost">>]}],
+    {ok, Component1} = escalus_client:start_component(Config, Options1),
+    Options2 = [{port, 8888}, {server, <<"localhost">>},
+                {from, <<"component2.localhost">>},
+                {bind_hostname, [H2 = <<"component2.localhost">>]}],
+    {ok, Component2} = escalus_client:start_component(Config, Options2),
+    escalus_client:send(Component1,
+                        escalus_stanza:chat(H1, H2,
+                                            Msg = <<"Yo!">>)),
+    escalus_assert:is_chat_message(
+      Msg, escalus_client:wait_for_stanza(Component2)),
+    [escalus_client:stop(C) || C <- [Component1, Component2]].
     
+multiple_bind(Config) ->
+    Hostnames = [<<"x.localhost">>, <<"y.localhost">>],
+    Options = [{port, 8888}, {server, <<"localhost">>},
+               {from, <<"component.localhost">>},
+               {bind_hostname, Hostnames}],
+    {ok, Component} = escalus_client:start_component(Config, Options),
+    {ok, Alice} = escalus_client:start(Config, alice, <<"res1">>),
+    Msgs = [begin
+                Msg = <<"Hi from ", H/binary>>,
+                escalus_client:send(Alice, escalus_stanza:chat_to(H, Msg)),
+                Msg
+            end || H <- Hostnames],
+    [escalus_assert:is_chat_message(
+       M, escalus_client:wait_for_stanza(Component))
+     || M <- Msgs],
+    [escalus_client:stop(C) || C <- [Component, Alice]].
+
+late_multiple_bind(Config) ->
+    Options = [{port, 8888}, {server, <<"localhost">>},
+               {from, <<"component.localhost">>},
+               {bind_hostname, [H1 = <<"h1.localhost">>]}],
+    {ok, Component} = escalus_client:start_component(Config, Options),
+    {ok, Alice} = escalus_client:start(Config, alice, <<"res1">>),
+    escalus_client:send(Alice, escalus_stanza:chat_to(H1, <<"Hi!">>)),
+    escalus_assert:is_chat_message(
+      <<"Hi!">>, escalus_client:wait_for_stanza(Component)),
+    H2 = <<"h2.localhost">>,
+    escalus_client:send(Component, escalus_stanza:component_bind(H2)),
+    escalus_client:wait_for_stanza(Component),
+    escalus_client:send(Alice, escalus_stanza:chat_to(H2, <<"Hi2!">>)),
+    escalus_assert:is_chat_message(
+      <<"Hi2!">>, escalus_client:wait_for_stanza(Component)),
+    [escalus_client:stop(C) || C <- [Component, Alice]].
+
+component_unbind(Config) ->
+    Options = [{port, 8888}, {server, <<"localhost">>},
+               {from, <<"component.localhost">>},
+               {bind_hostname, [H = <<"component1.localhost">>]}],
+    {ok, Component} = escalus_client:start_component(Config, Options),
+    {ok, Alice} = escalus_client:start(Config, alice, <<"res1">>),
+    escalus_client:send(Alice, escalus_stanza:chat_to(H, <<"Hi!">>)),
+    escalus_assert:is_chat_message(
+      <<"Hi!">>, escalus_client:wait_for_stanza(Component)),
+    escalus_client:send(Component, escalus_stanza:component_unbind(H)),
+    escalus:assert(is_iq_result, [],
+                   escalus_client:wait_for_stanza(Component)),
+    escalus_client:send(Alice, escalus_stanza:chat_to(H, <<"Hi!">>)),
+    escalus:assert(is_error, [<<"cancel">>, <<"service-unavailable">>],
+                   escalus_client:wait_for_stanza(Alice)),
+    [escalus_client:stop(C) || C <- [Component, Alice]].
